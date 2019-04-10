344eb78a20501a69603a5dce601b2117a441cd68
Sergey Samohvalov <Samohvalov@tlpt.ru>
2019-04-10 19:31:47 +0300

    Fix conflict

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-
-export default class App extends React.Component {
-	render() {
-		return (
-			<div className="App">
-				<p className="description">Миру-мир, студентам - beer.</p>
-			</div>
-		)
-	}
-}
-=======
 import React from 'react'
 
 export default () => <p>Homework app</p>
->>>>>>> cd0128e73701628872e98c8fb07dd4fd4f94ee50
--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import App from './components/App';
-=======
 import Form from './components/Form';
->>>>>>> cd0128e73701628872e98c8fb07dd4fd4f94ee50
 import './index.css';
 
 ReactDOM.render(<Form />, document.getElementById('root'));
